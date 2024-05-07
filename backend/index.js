14f9000e48825c79eb30de70e3f908c9ae59b329
Y-leeeee <lyr890802@gmail.com>
2024-05-07 17:31:55 +0200

    First commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+  path = require("path");
+cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/api", (_request, response) => {
+  response.send({ hello: "World" });
+});
+
+app.use(express.static(path.join(path.resolve(), "dist")));
+
+app.listen(3000, () => {
+  console.log("Redo på http://localhost:3000/");
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((result) => {
+        alert(`Hello ${result.hello}!`);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>Hello Youri Lee</div>
+      <p className="read-the-docs"></p>
+    </>
+  );
+}
+
+export default App;
